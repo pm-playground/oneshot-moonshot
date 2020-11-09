9f9730b25888eb08e476901dc93eca50f7c7ff68
Plamen Mitev <mitev@pop-os.localdomain>
2020-11-09 15:01:50 +0100

    Add authentication

--- routes/main.js
+++ routes/main.js
@@ -8,18 +8,30 @@ router.get('/status', (req, res, next) => {
   res.json({'status': 'ok'});
 })
 
-//* <-.login signup.->
+//* <-signup.->
 router.post('/signup', asyncMiddleware( async(req, res, next) => {
  const { name, email, password } = req.body;
  await UserModel.create({ email, password, name });
  res.status(200).json({'status': 'ok'});
 }));
 
-router.post('/login', (req, res, next)=> {
-  res.status(200);
-  res.json({'status': 'ok'})
-})
+//* <-login.->
+router.post('/login', asyncMiddleware( async(req, res, next) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({email});
+  if(!user) {
+    res.status(401).json({'message': 'unauthenticated'});
+    return;
+  }
+  const validate = await user.isValidPassword(password);
+  if(!validate) {
+    res.status(401).json({'message': 'unauthenticated'});
+    return;
+  }
+  res.status(200).json({"status": "ok"})
+}))
 
+//* <-logout.->
 router.post('/logout', (req, res, next)=> {
   res.status(200);
   res.json({'status': 'ok'})
