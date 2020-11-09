73eb2c13d2c23de90f7465ceb6bfb4f09c186d96
Plamen Mitev <mitev@pop-os.localdomain>
2020-11-09 14:18:50 +0100

    Adding user schema

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/main');
 const secureRoutes = require('./routes/secure');
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGO_URL;
+
+mongoose.connect(uri,{
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+  process.exit(1);
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDb');
+});
 
 const app = express();
 
--- routes/main.js
+++ routes/main.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const asyncMiddleware = require('../middleware/asyncMiddleware');
+const UserModel = require('../models/userModel');
 
-router.get('/status', (req,res) => {
+router.get('/status', (req, res, next) => {
   res.status(200);
   res.json({'status': 'ok'});
 })
 
 //* <-.login signup.->
-router.post('/signup', (req,res)=> {
+router.post('/signup', asyncMiddleware( async(req, res, next) => {
+ const { name, email, password } = req.body;
+ await UserModel.create({ email, password, name });
+ res.status(200).json({'status': 'ok'});
+}));
+
+router.post('/login', (req, res, next)=> {
   res.status(200);
   res.json({'status': 'ok'})
 })
 
-router.post('/login', (req,res)=> {
+router.post('/logout', (req, res, next)=> {
   res.status(200);
   res.json({'status': 'ok'})
 })
-
-router.post('/logout', (req,res)=> {
-  res.status(200);
-  res.json({'status': 'ok'})
-})
-router.post('/token', (req,res)=> {
+router.post('/token', (req, res, next)=> {
   res.status(200);
   res.json({'status': 'ok'})
 })
